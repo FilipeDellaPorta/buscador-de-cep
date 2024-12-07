d87877ed82d77dfd3e45e208014bdd62bce7686f
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-07 19:31:21 -0300

    melhorado o código que havia sido sugerido pela ide

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -12,20 +12,19 @@ public class ConsultaCep {
     public Endereco buscaEndereco(String cep) {
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient
+            HttpResponse<String>  response = HttpClient
                     .newHttpClient()
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Não consegui obter endereço a partir deste cep");
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
+
     }
 }
