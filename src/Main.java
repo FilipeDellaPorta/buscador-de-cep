e258463977d67e45698a06b43bf5576b3dceb0d5
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-12-07 19:34:26 -0300

    colocado o try catch para verificar o cep passado

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("01001001");
-        System.out.println(novoEndereco);
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco("01001001");
+            System.out.println(novoEndereco);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
     }
 }
